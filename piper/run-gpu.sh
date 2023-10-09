fc9ab2be6c6b840d58df72f2c759f836e405c766
Jason Dunn <William.Dunn@Quasarke.com>
2023-10-09 11:52:42 -0400

    remove new line indicators
    Signed-off-by: Jason Dunn <William.Dunn@Quasarke.com>

--- piper/run-gpu.sh
+++ piper/run-gpu.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-python3 -m wyoming_piper \
-    --piper '/usr/share/piper/piper' \
-    --cuda \
-    --uri 'tcp://0.0.0.0:10200' \
-    --data-dir /data \
-    --download-dir /data "$@"
+python3 -m wyoming_piper --piper '/usr/share/piper/piper' --cuda --uri 'tcp://0.0.0.0:10200' --data-dir /data --download-dir /data "$@"
--- whisper/run.sh
+++ whisper/run.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-python3 -m wyoming_faster_whisper \
-    --uri 'tcp://0.0.0.0:10300' \
-    --data-dir /data \
-    --download-dir /data "$@"
+python3 -m wyoming_faster_whisper --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data "$@"
